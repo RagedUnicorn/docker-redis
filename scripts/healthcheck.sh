8fb29a5844bbe7f3edf8e48d5639e70108d77002
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-05-11 02:02:28 +0200

    Update healthscript to work with stack file * When redis is configured to use a password the healthcheck script needs to use that same password

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-set -eo pipefail
+set -eox pipefail
 
 host="127.0.0.1"
+redis_secret="/run/secrets/com.ragedunicorn.redis.password"
 
-if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
-	exit 0
+if [ -f "${redis_secret}" ]; then
+	if ping="$(redis-cli -h "$host" -a $(cat "${redis_secret}") ping)" && [ "$ping" = 'PONG' ]; then
+		exit 0
+	fi
+else
+	if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
+		exit 0
+	fi
 fi
 
 exit 1
